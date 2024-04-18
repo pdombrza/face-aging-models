2b263bde1c5c4ce5a6c75e822795692c3171f890
Paweł Dombrzalski <pawel.dombrzalski@gmail.com>
2024-04-18 03:59:04 +0200

    Add gender to CACD dataset

---
+++ annotate_manual_cacd.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from copy import deepcopy
+
+CACD_PATH = "cacd_meta/CACD_features_sex.csv"
+MANUAL_READ = "cacd_meta/manual.csv"
+
+def main():
+    save = True
+
+    annotated = pd.read_csv(MANUAL_READ)
+    cacd_features = deepcopy(pd.read_csv(CACD_PATH))
+    cacd_features['name_only'] = cacd_features['name'].map(lambda x: x.split("_")[1])
+
+    merged_df = pd.merge(cacd_features, annotated, left_on=cacd_features['name_only'], right_on=annotated['name'], how='left')
+    merged_df['gender'] = merged_df['gender_x'].fillna('') + merged_df['gender_y'].fillna('')
+    merged_df = merged_df.drop(["name_only", "key_0", "Unnamed: 0", "name_y", "gender_x", "gender_y"], axis=1)
+    merged_df.rename(columns={'name_x': 'name'}, inplace=True)
+    merged_df['gender'] = merged_df['gender'].replace('', np.nan)
+
+    nan_count = merged_df['gender'].isna().sum()
+    nan_rows = merged_df[merged_df['gender'].isna()]
+    print(merged_df.head())
+    # print(merged_df)
+
+    if save:
+        pd.DataFrame.to_csv(merged_df, "cacd_meta/CACD_features_sex.csv")
+
+
+if __name__ == "__main__":
+    main()
