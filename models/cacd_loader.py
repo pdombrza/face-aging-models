c193b9f5732c702933da0ae2de947d86e0f3fdc9
Paweł Dombrzalski <pawel.dombrzalski@gmail.com>
2024-06-29 11:25:40 +0200

    Add cacd dataloader for cyclegan

--- models/cacd_loader.py
+++ models/cacd_loader.py
@@ -2,6 +2,7 @@ import os
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
+from torchvision.io import ImageReadMode, read_image
 import pandas as pd
 
 
@@ -30,6 +31,35 @@ class CACDDataset(Dataset):
             image = self.transform(image)
 
         return celeb_name, image, person_age, person_gender
+
+class CACDCycleGANDataset(Dataset):
+    def __init__(self, csv_file, img_root_dir, transform=None):
+        self.metadata = pd.read_csv(csv_file)
+        self.transform = transform
+        self.img_root_dir = img_root_dir
+        self.young_images = self.metadata[(self.metadata['age'] >= 20 & self.metadata['age'] <= 30)]
+        self.old_images = self.metadata[(self.metadata['age'] >= 50 & self.metadata['age'] <= 60)]
+
+    def __len__(self):
+        return min(len(self.young_images), len(self.old_images))
+
+    def __getitem__(self, index):
+        young_img_name = self.young_images.iloc[index]['name']
+        young_celeb_name = ''.join(young_img_name.split('_')[1:-1])
+        young_dir_path = os.path.join(self.img_root_dir, young_celeb_name)
+        young_image = read_image(os.path.join(young_dir_path, young_img_name), mode=ImageReadMode.RGB)
+
+        old_img_name = self.old_images.iloc[index]['name']
+        old_celeb_name = ''.join(old_img_name.split('_')[1:-1])
+        old_dir_path = os.path.join(self.img_root_dir, old_celeb_name)
+        old_image = read_image(os.path.join(old_dir_path, old_img_name), mode=ImageReadMode.RGB)
+
+        if self.transform is not None:
+            young_image = self.transform(young_image)
+            old_image = self.transform(old_image)
+
+        return young_image, old_image
+
 
 
 def main():
