ff3b5eebfea6efafa386aa2a8a8567359e1be1d8
Paweł Dombrzalski <01169161@pw.edu.pl>
2024-08-09 18:55:14 +0200

    PatchGAN discriminator for FRAN

--- src/models/FRAN/fran.py
+++ src/models/FRAN/fran.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class BlurUpsample(nn.Module):
     def __init__(self, blur_kernel_size=(3, 3), blur_kernel_sigma=(1.5, 1.5), upsample_scale=(2, 2)):
         self.blur_upsample = nn.Sequential(
-            kornia.filters.GaussianBlur2d(blur_kernel_size),
+            kornia.filters.GaussianBlur2d(blur_kernel_size, blur_kernel_sigma),
             nn.Upsample(scale_factor=upsample_scale, align_corners=False)
         )
 
@@ -76,3 +76,30 @@ class Generator(nn.Module):
 
     def forward(self, x):
         return self.generator(x)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels, num_channels=64, num_blocks=3, normalization=True):
+        super(Discriminator, self).__init__()
+        self.num_channels = num_channels
+        self.disc = [
+            nn.Conv2d(in_channels, self.num_channels, kernel_size=3, stride=1, padding=1),
+        ]
+        if normalization:
+            self.disc.append(nn.InstanceNorm2d(64))
+        self.disc.append(nn.LeakyReLU(0.2, inplace=True))
+
+        for _ in range(num_blocks):
+            layers = [nn.Conv2d(self.num_channels, 2 * self.num_channels, kernel_size=3, stride=1, padding=1)]
+            if normalization:
+                layers.append(nn.InstanceNorm2d(2 * self.num_channels))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            self.num_channels *= 2
+            self.disc += layers
+
+        self.disc.append(nn.Conv2d(num_channels, 1, kernel_size=3, stride=1, padding=1))
+
+        self.discriminator = nn.Sequential(*self.disc)
+
+    def forward(self, x):
+        return self.discriminator(x)
