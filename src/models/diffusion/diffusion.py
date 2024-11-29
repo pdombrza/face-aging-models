2c88cbf9e458d1d9bbde53059dfdb9200eda2b60
Paweł Dombrzalski <pawel.dombrzalski@gmail.com>
2024-11-29 04:08:56 +0100

    Start working on diffusion training loop

---
+++ src/models/diffusion/diffusion.py
@@ -0,0 +1,74 @@
+import torch
+import torchvision
+import torch.optim as optim
+import torch.nn as nn
+import lightning as L
+from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch.callbacks import ModelCheckpoint
+
+from src.models.diffusion.sampler import DDPM
+from src.models.diffusion.unet import UNet
+
+
+class DiffusionModel(L.LightningModule):
+    def __init__(self,
+                 generator: UNet | None = None,
+                 sampler: DDPM | None = None,
+                 lambda_cycle: float | None = None,
+    ) -> None:
+        super(DiffusionModel, self).__init__()
+        self.g_a_b = generator if generator is not None else UNet()
+        self.g_b_a = generator if generator is not None else UNet()
+        self.diffusion_sampler = sampler if sampler is not None else DDPM()
+        self.lambda_cycle = lambda_cycle
+        self.diffusion_loss = nn.L1Loss()
+        self.cycle_consistency_loss = nn.L1Loss()
+        self.automatic_optimization = False
+
+    def training_step(self, batch: torch.Tensor, batch_idx: int) -> None:
+        g_a_b_optimizer, g_b_a_optimizer = self.optimizers()
+
+        g_a_b_optimizer.zero_grad()
+        g_b_a_optimizer.zero_grad()
+
+        x_a = batch["young_image"]
+        x_b = batch["young_image"]
+        batch_size = x_a.size(0)
+        timestep = torch.randint(0, self.diffusion_sampler.n_timesteps (batch_size,), device=self.device)
+
+        noisy_x_a = self.diffusion_sampler.add_noise(x_a, timestep)
+        noisy_x_b = self.diffusion_sampler.add_noise(x_b, timestep)
+
+        pred_noise_a = self.g_b_a(noisy_x_b, timestep)
+        pred_noise_b = self.g_a_b(noisy_x_a, timestep)
+
+        loss_ddpm_a = self.diffusion_loss(pred_noise_a, noisy_x_a)
+        loss_ddpm_b = self.diffusion_loss(pred_noise_b, noisy_x_b)
+
+        # cycle consistency
+        reconstructed_x_a = self.diffusion_sampler.step(timestep, noisy_x_b, pred_noise_a)
+        reconstructed_x_b = self.diffusion_sampler.step(timestep, noisy_x_a, pred_noise_b)
+
+        cyc_loss = self.cycle_consistency_loss(x_a, reconstructed_x_a) + self.cycle_consistency_loss(x_b, reconstructed_x_b)
+        loss = loss_ddpm_a + loss_ddpm_b + self.lambda_cycle * cyc_loss
+
+        self.manual_backward(loss)
+        g_a_b_optimizer.step()
+        g_b_a_optimizer.step()
+
+        # logging
+        self.log_dict(
+            {
+                "loss_ddpm_a": loss_ddpm_a,
+                "loss_ddpm_b": loss_ddpm_b,
+                "cycle_consistency_loss": cyc_loss,
+                "total_loss": loss,
+            },
+            prog_bar=True
+        )
+        return
+
+    def configure_optimizers(self) -> list:
+        g_a_b_optimizer = optim.Adam(self.g_a_b.parameters(), lr=1e-5, betas=(0.5, 0.999))
+        g_b_a_optimizer = optim.Adam(self.g_b_a.parameters(), lr=1e-5, betas=(0.5, 0.999))
+        return [g_a_b_optimizer, g_b_a_optimizer]
--- src/models/diffusion/sampler.py
+++ src/models/diffusion/sampler.py
@@ -4,41 +4,27 @@ import numpy as np
 
 
 class DDPM:
-    def __init__(self, generator: torch.Generator, n_training_steps: int = 1000, beta_start: float = 0.00085, beta_end: float = 0.0120):
-        self.betas = torch.linspace(beta_start ** 0.5, beta_end ** 0.5, n_training_steps, dtype=torch.float32) ** 2  # huggingface scaled linear schedule
+    def __init__(self, beta_start: float = 1e-4, beta_end: float = 1e-2, n_timesteps: int = 1000):
+        self.betas = torch.linspace(beta_start ** 0.5, beta_end ** 0.5, n_timesteps, dtype=torch.float32) ** 2  # huggingface scaled linear schedule
         self.alphas = 1.0 - self.betas
         self.alpha_bars = torch.cumprod(self.alphas, dim=0)
-        self.generator = generator
-        self.n_training_steps = n_training_steps
-        self.timesteps = torch.from_numpy(np.arange(0, n_training_steps)[::-1].copy())
+        self.n_timesteps = n_timesteps
+        self.timesteps = torch.from_numpy(np.arange(0, n_timesteps)[::-1].copy())
 
-    def set_inference_steps(self, n_inference_steps: int = 50) -> None:
-        self.n_inference_steps = n_inference_steps
-        # 999, 998, ..., 0 => 1000 steps to 999, 979, 959, ... , 0 => 50 timesteps
-        step_ratio = self.n_training_steps // self.n_inference_steps
-        self.timesteps = torch.from_numpy((np.arange(0, n_inference_steps) * step_ratio).round()[::-1].copy().astype(np.int64))
-
-    def add_noise(self, original_samples: torch.FloatTensor, timesteps: torch.IntTensor) -> torch.FloatTensor:
+    @torch.no_grad()
+    def add_noise(self, original_samples: torch.FloatTensor, timesteps: torch.IntTensor) -> torch.FloatTensor:  # forward process
         alpha_bars = self.alpha_bars.to(device=original_samples.device, dtype=original_samples.dtype)
         timesteps = timesteps.to(device=original_samples.device)
-
-        sqrt_alpha_bars = torch.sqrt(alpha_bars[timesteps])
-        sqrt_alpha_bars = sqrt_alpha_bars.flatten()
-        while len(sqrt_alpha_bars.shape) < len(original_samples.shape):
-            sqrt_alpha_bars = sqrt_alpha_bars.unsqueeze(-1)
-
+        sqrt_alpha_bars = torch.sqrt(alpha_bars[timesteps]) # mean
         sqrt_one_minus_alpha_bars = torch.sqrt((1.0 - sqrt_alpha_bars[timesteps]))  # stdev
-        sqrt_one_minus_alpha_bars = sqrt_one_minus_alpha_bars.flatten()
-        while len(sqrt_one_minus_alpha_bars.shape) < len(sqrt_one_minus_alpha_bars.shape):
-            sqrt_one_minus_alpha_bars = sqrt_one_minus_alpha_bars.unsqueeze(-1)
-
         # equation (4) of the DDPM paper
         # X = mean + stdev * Z
-        noise = torch.randn(original_samples.shape, generator=self.generator, device=original_samples.device, dtype=original_samples.dtype)
+        noise = torch.randn(original_samples.shape, device=original_samples.device, dtype=original_samples.dtype)
         noisy_samples = (sqrt_alpha_bars * original_samples) + (sqrt_one_minus_alpha_bars) * noise
         return noisy_samples
 
-    def step(self, timestep: int, latent_image: torch.Tensor, model_output: torch.Tensor):
+    @torch.no_grad()
+    def step(self, timestep: int, latent_image: torch.Tensor, model_output: torch.Tensor):  # reverse process
         # model output - noise predicted by the UNet
         t = timestep
         prev_t = self._get_previous_timestep(t)
@@ -70,11 +56,6 @@ class DDPM:
         # X = mu + sigma * Z where Z ~ N(0, 1)
         pred_prev_sample += variance
         return pred_prev_sample
-
-    def set_strength(self, strength: float=1.0) -> None:
-        start_step = self.n_inference_steps - int(self.n_inference_steps * strength)
-        self.timesteps = self.timesteps[start_step:]
-        self.start_step = start_step
 
     def _get_variance(self, timestep: int) -> torch.Tensor:
         t = timestep
