9aa9ad8906a213fbf2591ec6ba615ff02c3915f9
Paweł Dombrzalski <pawel.dombrzalski@gmail.com>
2025-01-16 16:03:34 +0100

    General generation script prototype

--- scripts/generate.py
+++ scripts/generate.py
@@ -16,19 +16,25 @@ class Gender(Enum):
     MALE = 'M'
     FEMALE = 'F'
 
+    def __str__(self):
+        return self.value
+
 
 class ModelType(Enum):
     CYCLEGAN = 'cyclegan'
     FRAN = 'fran'
     DIFFUSION = 'diffusion'
 
+    def __str__(self):
+        return self.value
+
 def main():
     parser = ArgumentParser(description="Generate images from model.")
-    parser.add_argument("--model_type", type=ModelType, help="Model type. Available: 'cyclegan', 'diffusion', 'fran'.", required=True)
+    parser.add_argument("--model_type", type=ModelType, help="Model type", required=True, choices=list(ModelType))
     parser.add_argument("--ckpt_path", type=Path, help="Path of model checkpoint to use.", required=True)
     parser.add_argument("--input_img", type=Path, help="Input image path.", required=True)
     parser.add_argument("--input_age", type=Path, help="Input image age.", required=True)
-    parser.add_argument("--input_gender", type=Gender, help="Input image gender. Available: 'M' - male, 'F' - female", required=True)
+    parser.add_argument("--input_gender", type=Gender, help="Input image gender", choices=list(Gender), required=True)
     parser.add_argument("--save", type=Path, help="Path to save image.", required=False)
     args = parser.parse_args()
 
